201a8173a0761a1d8bab9450746fcd45ba6d6019
Ellen Gummesson <ellengummesson@gmail.com>
2015-05-02 18:14:15 +0200

    Add `echo`.

--- scripts/node.cmd
+++ scripts/node.cmd
@@ -4,6 +4,7 @@ setlocal EnableExtensions EnableDelayedExpansion
 set vimpath=%USERPROFILE%\vimfiles
 set plugpath=%vimpath%\plugged\vim-nodejs-complete\after\autoload
 
+echo Updating completion data...
 cd /d %plugpath%
 git update-index --assume-unchanged nodejs-doc.vim
 node update-nodejs-doc.js
--- scripts/node.sh
+++ scripts/node.sh
@@ -5,6 +5,7 @@ set -e
 vimpath=$HOME/.vim
 plugpath=$vimpath/plugged/vim-nodejs-complete/after/autoload
 
+echo "Updating completion data..."
 cd $plugpath
 git update-index --assume-unchanged nodejs-doc.vim
 node update-nodejs-doc.js
