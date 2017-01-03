20d2fff91f130a0318fda66c2fa31873aefec809
Ellen Gummesson <ellengummesson@gmail.com>
2017-01-03 19:53:41 +0100

    Change path.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,24 +5,24 @@ set -e
 userpath="${HOME}"
 vimpath="${HOME}/.vim"
 nvimpath="${HOME}/.config/nvim"
-gitpath="${HOME}/Git/vimfiles"
+gitpath="${HOME}/Code/vimfiles"
 
 echo "Creating folders..."
-mkdir -p $vimpath/autoload
-mkdir -p $vimpath/undo
-mkdir -p $vimpath/spell
+mkdir -p "${vimpath}/autoload"
+mkdir -p "${vimpath}/undo"
+mkdir -p "${vimpath}/spell"
 
 echo "Installing vim-plug..."
 curl --fail --location --output \
-  $vimpath/autoload/plug.vim \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  "${vimpath}/autoload/plug.vim" \
+  "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 echo "Symlinking files..."
-ln -sf $gitpath/.vimrc $userpath/.vimrc
-ln -sf $gitpath/.gvimrc $userpath/.gvimrc
-ln -sf $gitpath/init.vim $vimpath/init.vim
+ln -sf "${gitpath}/.vimrc" "${userpath}/.vimrc"
+ln -sf "${gitpath}/.gvimrc" "${userpath}/.gvimrc"
+ln -sf "${gitpath}/init.vim" "${vimpath}/init.vim"
 
 echo "Symlinking folders..."
-ln -sf $gitpath/snippets $vimpath/UltiSnips
-ln -sf $gitpath/after $vimpath/after
-ln -sf $vimpath $nvimpath
+ln -sf "${gitpath}/snippets" "${vimpath}/UltiSnips"
+ln -sf "${gitpath}/after" "${vimpath}/after"
+ln -sf "${vimpath}" "${nvimpath}"
