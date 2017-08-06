b09035be9dd0aae119dbfe982140d14705e1e1e4
Ellen Gummesson <ellengummesson@gmail.com>
2017-08-06 22:58:43 +0200

    Cleanup.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,27 +2,27 @@
 
 set -e
 
-userpath="${HOME}"
-vimpath="${HOME}/.vim"
-nvimpath="${HOME}/.config/nvim"
-gitpath="${HOME}/Code/vimfiles"
+userpath="$HOME"
+vimpath="$HOME/.vim"
+nvimpath="$HOME/.config/nvim"
+gitpath="$HOME/Code/vimfiles"
 
 echo "Creating folders..."
-mkdir -p "${vimpath}/autoload"
-mkdir -p "${vimpath}/undo"
-mkdir -p "${vimpath}/spell"
+mkdir -p "$vimpath/autoload"
+mkdir -p "$vimpath/undo"
+mkdir -p "$vimpath/spell"
 
 echo "Installing vim-plug..."
 curl --fail --location --output \
-  "${vimpath}/autoload/plug.vim" \
+  "$vimpath/autoload/plug.vim" \
   "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 echo "Symlinking files..."
-ln -sf "${gitpath}/.vimrc" "${userpath}/.vimrc"
-ln -sf "${gitpath}/.gvimrc" "${userpath}/.gvimrc"
-ln -sf "${gitpath}/init.vim" "${vimpath}/init.vim"
+ln -sf "$gitpath/.vimrc" "$userpath/.vimrc"
+ln -sf "$gitpath/.gvimrc" "$userpath/.gvimrc"
+ln -sf "$gitpath/init.vim" "$vimpath/init.vim"
 
 echo "Symlinking folders..."
-ln -sf "${gitpath}/snippets" "${vimpath}/UltiSnips"
-ln -sf "${gitpath}/after" "${vimpath}/after"
-ln -sf "${vimpath}" "${nvimpath}"
+ln -sf "$gitpath/snippets" "$vimpath/UltiSnips"
+ln -sf "$gitpath/after" "$vimpath/after"
+ln -sf "$vimpath" "$nvimpath"
